2397b20202b5009abb89bba9741dae25630039bb
Kazamas1 <eugenilos@gmail.com>
2022-04-10 23:28:04 +0200

    Limpieza estariones
    Limpiado estaciones para calcular días sin lluva

--- DBLimpio.sql
+++ DBLimpio.sql
@@ -22,3 +22,18 @@ Set Lluvia10d = sum(LuviaTotal) over (partition by Estacion order by Fecha rows 
 ,Lluvia2d = sum(LuviaTotal) over (partition by Estacion order by Fecha rows BETWEEN 2 PRECEDING and current row)
 
 
+with delta as (
+Select
+id 
+,sum(LuviaTotal) over (partition by Estacion order by Fecha rows BETWEEN 10 PRECEDING and current row) as d10
+,sum(LuviaTotal) over (partition by Estacion order by Fecha rows BETWEEN 5 PRECEDING and current row) as d5
+,sum(LuviaTotal) over (partition by Estacion order by Fecha rows BETWEEN 2 PRECEDING and current row) as d2
+from DA_Baleares_Limpios
+)
+update DA_Baleares_Limpios 
+inner join delta 
+on delta.id = DA_Baleares_Limpios.id
+/*and delta.Estacion = DA_Baleares_Limpios.Estacion */
+set Lluvia10d = d10,
+ Luvia5d = d5,
+Lluvia2d = d2
